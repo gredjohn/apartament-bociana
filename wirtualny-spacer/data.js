0d2aef90ae3b75d48fe8fd6664581ebd2d7ff3bb
greg <gwiechec@o2.pl>
2024-05-06 00:25:18 +0200

    Add virtual walk

---
+++ wirtualny-spacer/data.js
@@ -0,0 +1,368 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-korytarz",
+      "name": "korytarz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.3718530627607315,
+        "pitch": 0.08036758513979514,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18193022763527367,
+          "pitch": 0.23528268768966853,
+          "rotation": 0,
+          "target": "5-azienka"
+        },
+        {
+          "yaw": 0.4262010893234187,
+          "pitch": 0.2884010308795375,
+          "rotation": 0,
+          "target": "3-sypiania"
+        },
+        {
+          "yaw": 2.380073076436899,
+          "pitch": 0.3044605463982837,
+          "rotation": 0,
+          "target": "4-gabinet"
+        },
+        {
+          "yaw": -3.0305928988464768,
+          "pitch": 0.23173940919339486,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.307932350630722,
+          "pitch": -0.05313980532449669,
+          "title": "Zapraszamy do kontaktu",
+          "text": "Mieszkanie już prawie gotowe. Zapraszamy do oglądania."
+        },
+        {
+          "yaw": -0.9985609046465402,
+          "pitch": -0.15555727447740253,
+          "title": "Duża zabudowana szafa",
+          "text": "W środku miejsce na pralkę i wieszaki na ubrania"
+        }
+      ]
+    },
+    {
+      "id": "1-salon",
+      "name": "salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.6057441369842866,
+        "pitch": 0.10134824356809702,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.726864534825431,
+          "pitch": 0.3556172463433498,
+          "rotation": 0,
+          "target": "0-korytarz"
+        },
+        {
+          "yaw": -2.082530435277988,
+          "pitch": 0.4255051165091057,
+          "rotation": 0,
+          "target": "2-kuchnia"
+        },
+        {
+          "yaw": -1.8435730622617932,
+          "pitch": 0.07123897634281562,
+          "rotation": 0,
+          "target": "6-balkon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0118468299664016,
+          "pitch": 0.11895362642477281,
+          "title": "Wyjątkowe połączenie lameli i lustra",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-kuchnia",
+      "name": "kuchnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0583897709293826,
+        "pitch": -0.0023083190687458455,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0639913611806415,
+          "pitch": 0.2802942994765125,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": 0.702758770532645,
+          "pitch": 0.1886905911157033,
+          "rotation": 0,
+          "target": "6-balkon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9315624055723752,
+          "pitch": 0.15318433288060618,
+          "title": "Przestronna kuchnia",
+          "text": "Ze zmywarką, piekarnikiem, lodówką, płytą indukcyją i nowoczesnym okapem."
+        }
+      ]
+    },
+    {
+      "id": "3-sypiania",
+      "name": "sypiania",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.011841103169159,
+        "pitch": 0.20307743665363986,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.239515576063411,
+          "pitch": 0.13657663337443005,
+          "rotation": 0,
+          "target": "0-korytarz"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08323130930444123,
+          "pitch": 0.22388865745532804,
+          "title": "Cicha sypialnia",
+          "text": "Z miejscem na łóżko 140cm oraz dwie szafi nocne"
+        }
+      ]
+    },
+    {
+      "id": "4-gabinet",
+      "name": "gabinet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8784507048275554,
+        "pitch": 0.15038494885871856,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5504413973189077,
+          "pitch": 0.2192030308624524,
+          "rotation": 0,
+          "target": "0-korytarz"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6673220575819165,
+          "pitch": 0.1477536801243069,
+          "title": "Gabinet? A może pokój dla dziecka?",
+          "text": "Obie aranżacje są jak najbardziej możliwe do zrealizowania."
+        }
+      ]
+    },
+    {
+      "id": "5-azienka",
+      "name": "łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.785193343167169,
+        "pitch": -0.07897474584543751,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.773304459696421,
+          "pitch": 0.3770338722691342,
+          "rotation": 0,
+          "target": "0-korytarz"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2174337745366088,
+          "pitch": -0.15385580086493533,
+          "title": "Duże lustro",
+          "text": "Duże podświetlane lustro"
+        }
+      ]
+    },
+    {
+      "id": "6-balkon",
+      "name": "balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0826420222112425,
+        "pitch": 0.1822896448319078,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.735022280369269,
+          "pitch": 0.1760684911551973,
+          "rotation": 0,
+          "target": "2-kuchnia"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0952285909370296,
+          "pitch": 0.26808173189104423,
+          "title": "Zaciszne miejsce",
+          "text": "Duży balkon otoczony drzewami"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
