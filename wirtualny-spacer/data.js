f2d94b2fbcc7d747f7d850c0c92e40b2602d0b4f
greg <gwiechec@o2.pl>
2024-05-25 00:18:17 +0200

    Update virtual walk

--- wirtualny-spacer/data.js
+++ wirtualny-spacer/data.js
@@ -24,48 +24,54 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "yaw": -2.3718530627607315,
-        "pitch": 0.08036758513979514,
+        "yaw": 0.5611903438343244,
+        "pitch": 0.0035271523186359843,
         "fov": 1.5396019870895838
       },
       "linkHotspots": [
         {
-          "yaw": -0.18193022763527367,
-          "pitch": 0.23528268768966853,
+          "yaw": 2.5509774925447335,
+          "pitch": -0.07252594331978912,
           "rotation": 0,
           "target": "5-azienka"
         },
         {
-          "yaw": 0.4262010893234187,
-          "pitch": 0.2884010308795375,
+          "yaw": 2.9887463346621495,
+          "pitch": -0.09236183277818455,
           "rotation": 0,
-          "target": "3-sypiania"
+          "target": "3-sypialnia"
         },
         {
-          "yaw": 2.380073076436899,
-          "pitch": 0.3044605463982837,
+          "yaw": -1.5743045247863048,
+          "pitch": 0.12975802489049748,
           "rotation": 0,
           "target": "4-gabinet"
         },
         {
-          "yaw": -3.0305928988464768,
-          "pitch": 0.23173940919339486,
+          "yaw": -0.3366215636414118,
+          "pitch": 0.01256779399408181,
           "rotation": 0,
           "target": "1-salon"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -2.307932350630722,
-          "pitch": -0.05313980532449669,
+          "yaw": 0.8292305512284361,
+          "pitch": -0.23787147099283956,
           "title": "Zapraszamy do kontaktu",
-          "text": "Mieszkanie już prawie gotowe. Zapraszamy do oglądania."
+          "text": "Z przyjemnością przedstawiam Państwu mieszkanie \"perełkę\"!<br>"
         },
         {
-          "yaw": -0.9985609046465402,
-          "pitch": -0.15555727447740253,
+          "yaw": 2.162968739886937,
+          "pitch": 0.0067430853150618475,
           "title": "Duża zabudowana szafa",
           "text": "W środku miejsce na pralkę i wieszaki na ubrania"
+        },
+        {
+          "yaw": -3.1412061256507364,
+          "pitch": -0.4691245511307578,
+          "title": "Loftowe lustro",
+          "text": "Idealnie podkreśla charakter korytarza"
         }
       ]
     },
@@ -93,36 +99,42 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "yaw": -2.6057441369842866,
-        "pitch": 0.10134824356809702,
+        "yaw": 0.053627810010770105,
+        "pitch": 0.39680463584782544,
         "fov": 1.5396019870895838
       },
       "linkHotspots": [
         {
-          "yaw": 2.726864534825431,
-          "pitch": 0.3556172463433498,
+          "yaw": -1.7150902284554714,
+          "pitch": 0.21119655394735304,
           "rotation": 0,
           "target": "0-korytarz"
         },
         {
-          "yaw": -2.082530435277988,
-          "pitch": 0.4255051165091057,
+          "yaw": -0.35390717329608634,
+          "pitch": 0.17020672715489304,
           "rotation": 0,
           "target": "2-kuchnia"
-        },
-        {
-          "yaw": -1.8435730622617932,
-          "pitch": 0.07123897634281562,
-          "rotation": 0,
-          "target": "6-balkon"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -3.0118468299664016,
-          "pitch": 0.11895362642477281,
+          "yaw": -1.3538742955393435,
+          "pitch": -0.09812599474970085,
           "title": "Wyjątkowe połączenie lameli i lustra",
-          "text": "Text"
+          "text": "Lustro wykonane na wymiar oraz lamele doskonale łączą kuchnię z salonem"
+        },
+        {
+          "yaw": 0.46859746466728325,
+          "pitch": 0.7813097667474143,
+          "title": "Stylowe stoliki",
+          "text": "Stoliki kawowe wyonane na zamówienie z drewna jesionowego. Są niepowtarzalne i znakomicie wpasowują się w przestrzeń salonu."
+        },
+        {
+          "yaw": 0.4588894367895442,
+          "pitch": 0.14757338228224626,
+          "title": "Wygodna kanapa",
+          "text": "Bardzo wygodna kanapa zaprojektowana oraz wykonana do tego mieszkania"
         }
       ]
     },
@@ -150,36 +162,42 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "yaw": -3.0583897709293826,
-        "pitch": -0.0023083190687458455,
+        "yaw": -0.9133162332324858,
+        "pitch": 0.02469006623053005,
         "fov": 1.5396019870895838
       },
       "linkHotspots": [
         {
-          "yaw": 3.0639913611806415,
-          "pitch": 0.2802942994765125,
-          "rotation": 0,
-          "target": "1-salon"
-        },
-        {
-          "yaw": 0.702758770532645,
-          "pitch": 0.1886905911157033,
+          "yaw": 2.3124777997841512,
+          "pitch": 0.01572123806882786,
           "rotation": 0,
           "target": "6-balkon"
+        },
+        {
+          "yaw": -1.4007853557537384,
+          "pitch": 0.12161560368005908,
+          "rotation": 0,
+          "target": "1-salon"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -1.9315624055723752,
-          "pitch": 0.15318433288060618,
-          "title": "Przestronna kuchnia",
-          "text": "Ze zmywarką, piekarnikiem, lodówką, płytą indukcyją i nowoczesnym okapem."
+          "yaw": 0.017951544154165333,
+          "pitch": 0.058238434445264886,
+          "title": "Nowoczesna i przestronna kuchnia",
+          "text": "Miejsce na kuchnię zostało wykorzystane do maksimum. W kuchni znajduję się lodówka, zmywarka, piekarnik, zlew i płyta oraz bardzo dużo szafek do przechowywania"
+        },
+        {
+          "yaw": 0.4916388738429198,
+          "pitch": -0.31764332635500026,
+          "title": "Duży okap",
+          "text": "Nadaje charakteru kuchni"
         }
       ]
     },
     {
-      "id": "3-sypiania",
-      "name": "sypiania",
+      "id": "3-sypialnia",
+      "name": "sypialnia",
       "levels": [
         {
           "tileSize": 256,
@@ -201,24 +219,30 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "yaw": 3.011841103169159,
-        "pitch": 0.20307743665363986,
+        "yaw": 1.3573291336663598,
+        "pitch": 0.03879867550512017,
         "fov": 1.5396019870895838
       },
       "linkHotspots": [
         {
-          "yaw": -2.239515576063411,
-          "pitch": 0.13657663337443005,
+          "yaw": 0.4344490240057617,
+          "pitch": 0.09503164790399055,
           "rotation": 0,
           "target": "0-korytarz"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.08323130930444123,
-          "pitch": 0.22388865745532804,
-          "title": "Cicha sypialnia",
-          "text": "Z miejscem na łóżko 140cm oraz dwie szafi nocne"
+          "yaw": 2.109694143311051,
+          "pitch": -0.0810487663352557,
+          "title": "Sypialnia w stylu hygge",
+          "text": "Odpowiednio dobrane dodatki, kolory oraz oświetlenie sprawią, że sypialnia będzie wyjątkową częścią mieszkania"
+        },
+        {
+          "yaw": -1.4673372612270121,
+          "pitch": -0.12955588963101405,
+          "title": "Możliwość zabudowy",
+          "text": "W sypialni nadal pozostaje miejsce na wykonanie dużej szafy"
         }
       ]
     },
@@ -246,24 +270,30 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "yaw": -2.8784507048275554,
-        "pitch": 0.15038494885871856,
+        "yaw": 0.8426892466448521,
+        "pitch": 0.14990522127996897,
         "fov": 1.5396019870895838
       },
       "linkHotspots": [
         {
-          "yaw": 2.5504413973189077,
-          "pitch": 0.2192030308624524,
+          "yaw": -0.11867229434242788,
+          "pitch": -0.08952498991008007,
           "rotation": 0,
           "target": "0-korytarz"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.6673220575819165,
-          "pitch": 0.1477536801243069,
-          "title": "Gabinet? A może pokój dla dziecka?",
-          "text": "Obie aranżacje są jak najbardziej możliwe do zrealizowania."
+          "yaw": -2.488603729109496,
+          "pitch": 0.03316693085915645,
+          "title": "Gabinet czy pokój dziecka?",
+          "text": "Pomieszczenie może być zarówno gabinetem jak i pokojem dziecięcym"
+        },
+        {
+          "yaw": 1.4500466131770473,
+          "pitch": 0.2130027955124021,
+          "title": "Wygodny fotel",
+          "text": "Model fotelu jest spójny z kanapą w salonie i może być wykorzystany jako komplet"
         }
       ]
     },
@@ -291,24 +321,36 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "yaw": 2.785193343167169,
-        "pitch": -0.07897474584543751,
+        "yaw": 1.2818568667626202,
+        "pitch": 0.10581456955942059,
         "fov": 1.5396019870895838
       },
       "linkHotspots": [
         {
-          "yaw": -2.773304459696421,
-          "pitch": 0.3770338722691342,
+          "yaw": 1.9603167238378738,
+          "pitch": -0.09271694376990425,
           "rotation": 0,
           "target": "0-korytarz"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 1.2174337745366088,
-          "pitch": -0.15385580086493533,
+          "yaw": -0.5144702135185284,
+          "pitch": -0.36837407616225093,
           "title": "Duże lustro",
-          "text": "Duże podświetlane lustro"
+          "text": "Duże lustro z podświetleniem LED"
+        },
+        {
+          "yaw": -1.2702256837617885,
+          "pitch": -0.11710063500358636,
+          "title": "Wanna z prysznicem",
+          "text": "Na stosunkowo niewielkiej przestrzeni łazienki udało się znaleźć kompromis pomiędzy wanną i prysznicem"
+        },
+        {
+          "yaw": -2.579302666286331,
+          "pitch": -0.32225408736066363,
+          "title": "Szafka nad toaletą",
+          "text": "Podświetlana szafka LED z dużą powierzchnią do przechowywania"
         }
       ]
     },
@@ -336,22 +378,22 @@ var APP_DATA = {
       ],
       "faceSize": 1520,
       "initialViewParameters": {
-        "yaw": -3.0826420222112425,
-        "pitch": 0.1822896448319078,
+        "yaw": 2.8251330480028507,
+        "pitch": 0.20013160944467856,
         "fov": 1.5396019870895838
       },
       "linkHotspots": [
         {
-          "yaw": 2.735022280369269,
-          "pitch": 0.1760684911551973,
+          "yaw": 2.7117868280815944,
+          "pitch": 0.0734800149717234,
           "rotation": 0,
           "target": "2-kuchnia"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -3.0952285909370296,
-          "pitch": 0.26808173189104423,
+          "yaw": -3.1317227548990196,
+          "pitch": 0.10829743444301343,
           "title": "Zaciszne miejsce",
           "text": "Duży balkon otoczony drzewami"
         }
@@ -362,7 +404,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": true
   }
 };
