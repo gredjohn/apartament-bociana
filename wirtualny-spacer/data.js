e89194c9093be02b385521cbe7e28818726344e2
Grzegorz Wiechec <grzegorz.wiechec@optimizely.com>
2024-07-05 14:21:28 +0200

    add fullscreen button

--- wirtualny-spacer/data.js
+++ wirtualny-spacer/data.js
@@ -430,7 +430,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": true
   }
 };
